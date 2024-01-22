dcb8515cf0988c9e1bd87cb8087717dc45b27be5
Dimase <zwere4ek@list.ru>
2024-01-22 16:08:08 +0300

    addTask1

---
+++ Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        while (true)
+        {
+            Console.WriteLine("Введите целое число (для завершения введите 'q'): ");
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "q")
+            {
+                Console.WriteLine("Программа завершена.");
+                break;
+            }
+
+            if (IsEvenSum(int.Parse(input)))
+            {
+                Console.WriteLine("Сумма цифр чётная. Программа завершена.");
+                break;
+            }
+        }
+    }
+
+    static bool IsEvenSum(int number)
+    {
+        int sum = 0;
+        foreach (char digit in number.ToString())
+        {
+            sum += int.Parse(digit.ToString());
+        }
+
+        return sum % 2 == 0;
+    }
+}

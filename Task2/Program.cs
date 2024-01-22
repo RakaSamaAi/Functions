b879dc26a27dbd8306d0452805ac1165fea49a40
Dimase <zwere4ek@list.ru>
2024-01-22 16:20:29 +0300

    addTask2

---
+++ Task2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        int arraySize = 10; 
+
+        int[] numbers = GenerateRandomNumbers(arraySize);
+
+        Console.WriteLine("Сгенерированный массив:");
+        PrintArray(numbers);
+
+        int evenCount = CountEvenNumbers(numbers);
+        Console.WriteLine($"Количество четных чисел в массиве: {evenCount}");
+    }
+
+    static int[] GenerateRandomNumbers(int size)
+    {
+        Random random = new Random();
+        int[] numbers = new int[size];
+
+        for (int i = 0; i < size; i++)
+        {
+            numbers[i] = random.Next(100, 1000);
+        }
+
+        return numbers;
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+
+    static int CountEvenNumbers(int[] array)
+    {
+        int count = 0;
+        foreach (var number in array)
+        {
+            if (number % 2 == 0)
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+

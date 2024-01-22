50c78e007827d84b33d12df58ac1b392bf70c50c
Dimase <zwere4ek@list.ru>
2024-01-22 16:29:45 +0300

    addTask3

---
+++ Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int[] numbers = { 1, 2, 3, 4, 5 };
+
+        Console.WriteLine("Исходный массив:");
+        PrintArray(numbers);
+
+        ReverseArray(numbers);
+
+        Console.WriteLine("Перевернутый массив:");
+        PrintArray(numbers);
+    }
+
+    static void ReverseArray(int[] array)
+    {
+        int length = array.Length;
+
+        for (int i = 0; i < length / 2; i++)
+        {
+            int temp = array[i];
+            array[i] = array[length - i - 1];
+            array[length - i - 1] = temp;
+        }
+    }
+
+    static void PrintArray(int[] array)
+    {
+        foreach (var number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+    }
+}
+
